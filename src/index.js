77bdd314245158de6257a97b874b2c7004831e8b
SushmithaLokesh <163681850+SushmithaLokesh@users.noreply.github.com>
2024-04-30 19:27:35 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import { useState } from "react";
+import ReactDOM from "react-dom/client";
+import RandomNumber from "./random";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<div>
+<RandomNumber></RandomNumber></div>
+)
